f5a6f74746348d392eec4c1587f9d3ce802e9fbf
GyatsoYT <nirusinghneesham@gmail.com>
2026-09-10 14:13:59 +0530

    Switched Datagen to Viriformat. bench: 1215504

--- Gyatso/src/datagen.nim
+++ Gyatso/src/datagen.nim
@@ -1,95 +1,7 @@
-
-import coretypes, bitboard, board, movegen, search, evaluate, attacks, zobrist,
-       tt, searchparams, history, nnuetypes, nnue
+import coretypes, bitboard, board, movegen, search, attacks, evaluate,
+       tt, searchparams, history, nnue, viriformat
 import std/[os, strutils, random, monotimes, times, atomics, cpuinfo,
-            parseopt, json, math]
-
-type
-  BinRecord* {.packed.} = object
-    packedBoard*: array[24, uint8]  ## bytes  0..23
-    sideToMove*:  uint8              ## byte   24
-    castlingRights*: uint8           ## byte   25
-    enPassantFile*: int8             ## byte   26
-    halfmoveClock*: uint8            ## byte   27
-    scoreCp*:     int16              ## bytes 28..29
-    wdl*:         float32            ## bytes 30..33
-    pieceCount*:  uint8              ## byte   34
-    padding*:     array[5, uint8]    ## bytes 35..39
-
-static:
-  doAssert sizeof(BinRecord) == 40, "BinRecord must be exactly 40 bytes"
-
-{.push inline.}
-
-proc pieceNibble(p: Piece): uint8 =
-  let colorBit: uint8 = if p.color == Black: 8'u8 else: 0'u8
-  let typeBits: uint8 = case p.pieceType
-    of Pawn:   1'u8
-    of Knight: 2'u8
-    of Bishop: 3'u8
-    of Rook:   4'u8
-    of Queen:  5'u8
-    of King:   6'u8
-    else:      0'u8
-  colorBit or typeBits
-
-proc packPositionFromBoard*(b: Board; scoreCp: int16; wdl: float32;
-                            outBuf: var BinRecord) =
-  zeroMem(addr outBuf, 40)
-
-  let occBB: Bitboard = b.occupied
-  let occupied: uint64 = occBB.uint64
-  outBuf.pieceCount = uint8(occBB.popcount())
-  copyMem(addr outBuf.packedBoard[0], unsafeAddr occupied, 8)
-
-  var occVar: Bitboard = occBB
-  var nibbleIdx = 0
-  while not occVar.isEmpty():
-    let sqIdx = occVar.lsb().int
-    occVar = occVar and (occVar - Bitboard(1))
-    let nibble = pieceNibble(b.mailbox[sqIdx])
-    let byteIdx = 8 + (nibbleIdx shr 1)
-    if (nibbleIdx and 1) == 0:
-      outBuf.packedBoard[byteIdx] = nibble
-    else:
-      outBuf.packedBoard[byteIdx] = outBuf.packedBoard[byteIdx] or (nibble shl 4)
-    inc nibbleIdx
-
-  outBuf.sideToMove     = if b.stm == White: 0'u8 else: 1'u8
-  outBuf.castlingRights = cast[uint8](b.castling)
-  outBuf.enPassantFile  = if b.epSquare == NoSquare: -1'i8
-                          else: int8(b.epSquare.file)
-  outBuf.halfmoveClock  = min(b.halfmove, 255'u8)
-  outBuf.scoreCp        = scoreCp
-  outBuf.wdl            = wdl
-
-{.pop.}
-
-const WdlScale = 400.0'f32
-
-func evalToWdl(scoreCp: int): float32 {.inline.} =
-  1.0'f32 / (1.0'f32 + exp(-scoreCp.float32 / WdlScale))
-
-const
-  BloomBits   = 1 shl 26
-  BloomMask   = BloomBits - 1
-  BloomBytes  = BloomBits shr 3
-
-var gBloomFilter {.global.}: array[BloomBytes, uint8]
-
-proc bloomCheck(key: uint64): bool {.inline.} =
-  let h1 = system.int((key * 0x9e3779b97f4a7c15'u64) shr 38) and BloomMask
-  let h2 = system.int((key * 0x6c62272e07bb0142'u64) shr 38) and BloomMask
-  let b1 = uint8(1) shl (h1 and 7)
-  let b2 = uint8(1) shl (h2 and 7)
-  (gBloomFilter[h1 shr 3] and b1) != 0 and
-  (gBloomFilter[h2 shr 3] and b2) != 0
-
-proc bloomInsert(key: uint64) {.inline.} =
-  let h1 = system.int((key * 0x9e3779b97f4a7c15'u64) shr 38) and BloomMask
-  let h2 = system.int((key * 0x6c62272e07bb0142'u64) shr 38) and BloomMask
-  gBloomFilter[h1 shr 3] = gBloomFilter[h1 shr 3] or (uint8(1) shl (h1 and 7))
-  gBloomFilter[h2 shr 3] = gBloomFilter[h2 shr 3] or (uint8(1) shl (h2 and 7))
+            parseopt, json]
 
 type
   OpeningBook = object
@@ -115,22 +27,6 @@ proc selectOpening(book: var OpeningBook; rng: var Rand): (int, string) {.inline
   let idx = book.fens.len - 1
   return (idx, book.fens[idx])
 
-type
-  BufferedPosition = object
-    record:   BinRecord
-    evalWdl:  float32
-
-type
-  GameResult = enum
-    grWhiteWin, grBlackWin, grDraw, grOngoing
-
-  AdjState = object
-    winCount:    int
-    winSide:     Color
-    drawStreak:  int
-
-var tNNUE {.threadvar.}: NNUEState
-
 proc searchPosition(b: var Board;
                     softNodes, hardNodes: uint64;
                     stopFlag: var Atomic[bool]): (Move, int) =
@@ -142,170 +38,111 @@ proc searchPosition(b: var Board;
   info.softLimitMs   = int64(high(int32))
   info.hardLimitMs   = int64(high(int32))
   info.depthLimit    = 0
-  info.nodeLimit     = hardNodes   # hard: abort mid-search
-  info.softNodeLimit = softNodes   # soft: exit after depth completes
+  info.nodeLimit     = hardNodes
+  info.softNodeLimit = softNodes
   info.nodes         = 0
   info.selDepth      = 0
   info.silent        = true
   info.stopFlag      = addr stopFlag
 
-  refreshNNUE(b, tNNUE)
-
   let (move, score) = iterativeDeepening(b, info)
   return (move, score)
 
-proc playGame(book: var OpeningBook;
-              rng: var Rand;
-              softNodes, hardNodes: uint64;
-              randPlies: int;
-              filterWindow: int;
-              lambda: float32;
-              stopFlag: var Atomic[bool];
-              positions: var seq[BufferedPosition]): GameResult =
-
-  positions.setLen(0)
+proc dgPlayGame(book: var OpeningBook;
+                rng: var Rand;
+                softNodes, hardNodes: uint64;
+                vb: var ViriBuffer): (int, bool) =
 
   var b: Board
+  var numRandom: int
+
   if book.fens.len > 0:
     let (bookIdx, fen) = selectOpening(book, rng)
     book.counts[bookIdx] += 1
     b = parseFen(fen)
+    numRandom = rng.rand(9)
   else:
     b = parseFen(StartPos)
+    numRandom = 8 + rng.rand(2)
 
-  let varietyTarget = rng.rand(randPlies) + 1
-  var varietyPlayed = 0
-  while varietyPlayed < varietyTarget:
-    var vml: MoveList
-    generateMoves(b, vml)
-    if vml.len == 0: break
-    let idx = rng.rand(vml.len - 1)
-    b.makeMove(vml.moves[idx])
-    inc varietyPlayed
-
-  var adj: AdjState
-  var moveCount = 0
-  var ply       = 0
-
-  while true:
-    inc moveCount
-    if moveCount > 300: return grDraw
-
-    # Draw detection
-    if b.isFiftyMove():        return grDraw
-    if b.isRepetition():       return grDraw
-    if b.isInsufficientMaterial(): return grDraw
-
+  for i in 0 ..< numRandom:
     var ml: MoveList
     generateMoves(b, ml)
-    if ml.len == 0:
+    if ml.len == 0: return (0, false)
+    let mv = ml.moves[rng.rand(ml.len - 1)]
+    b.makeMove(mv)
+
+  vb.reset()
+  vb.writeBoard(b)
+
+  var numEntries  = 0
+  var moveCount   = 0
+  var winCount    = 0
+  var winSide     = White
+  var drawStreak  = 0
+  var gameOutcome = 0.5'f64
+  var localStop: Atomic[bool]
+  localStop.store(false, moRelaxed)
+
+  for ply in 0 ..< 512:
+    inc moveCount
+
+    if b.isInsufficientMaterial() or b.halfmove >= 100 or b.isRepetition():
+      gameOutcome = 0.5
+      break
+
+    if moveCount > 300:
+      gameOutcome = 0.5
+      break
+
+    let (bestMove, scoreStm) = searchPosition(b, softNodes, hardNodes, localStop)
+
+    if bestMove == NullMove:
       if not b.checkers.isEmpty():
-        return if b.stm == White: grBlackWin else: grWhiteWin
+        gameOutcome = if b.stm == White: 0.0 else: 1.0
       else:
-        return grDraw
+        gameOutcome = 0.5
+      break
 
-    let (bestMove, scoreStm) = searchPosition(b, softNodes, hardNodes, stopFlag)
-    if bestMove == NullMove: return grDraw
+    let whiteScore = if b.stm == White: scoreStm else: -scoreStm
+    let absScore   = abs(scoreStm)
 
-    let absScore = abs(scoreStm)
+    if ply == 0 and absScore > 400:
+      return (0, false)
+
+    vb.writeMoveEval(bestMove, whiteScore)
+    inc numEntries
 
     if absScore >= 2500:
       let winningSide = if scoreStm > 0: b.stm else: b.stm.opposite()
-      if adj.winCount > 0 and adj.winSide == winningSide:
-        inc adj.winCount
+      if winCount > 0 and winSide == winningSide:
+        inc winCount
       else:
-        adj.winCount = 1
-        adj.winSide  = winningSide
-      adj.drawStreak = 0
+        winCount = 1
+        winSide  = winningSide
+      drawStreak = 0
     elif moveCount > 80 and absScore <= 15 and b.halfmove >= 30:
-      inc adj.drawStreak
-      adj.winCount = 0
+      inc drawStreak
+      winCount = 0
     else:
-      adj.winCount   = 0
-      adj.drawStreak = 0
+      winCount   = 0
+      drawStreak = 0
 
-    if adj.winCount >= 4:
-      return if adj.winSide == White: grWhiteWin else: grBlackWin
-    if adj.drawStreak >= 10:
-      return grDraw
-
-    let staticEval = evaluate(b, tNNUE)
+    if winCount >= 4:
+      gameOutcome = if winSide == White: 1.0 else: 0.0
+      break
+    if drawStreak >= 10:
+      gameOutcome = 0.5
+      break
 
     b.makeMove(bestMove)
-    inc ply
 
-    let givesCheck = not b.checkers.isEmpty()
+  if numEntries == 0: return (0, false)
 
-    let scoreDelta = abs(staticEval - scoreStm)
-    let passFilter = filterWindow <= 0 or scoreDelta <= filterWindow
+  let wdl: uint8 = if gameOutcome == 1.0: 2'u8 elif gameOutcome == 0.0: 0'u8 else: 1'u8
+  vb.patchWdl(wdl)
 
-    let moveIsQuiet = bestMove.isQuiet() and
-                      (b.history[b.histLen - 1].captured == NoPiece)
-
-    if moveIsQuiet and
-       not givesCheck and
-       absScore <= 2500 and
-       ply >= 16 and
-       passFilter:
-
-      let pc = b.occupied.popcount()
-      if pc >= 3 and pc <= 32:
-        let hk = b.hash.uint64
-        if not bloomCheck(hk):
-          bloomInsert(hk)
-
-          let weAre = b.stm.opposite()
-
-          let scoreCpWhite: int16 =
-            if weAre == White: int16(scoreStm) else: int16(-scoreStm)
-
-          let ew = evalToWdl(scoreStm)
-          let evalWdlWhite: float32 =
-            if weAre == White: ew else: 1.0'f32 - ew
-
-          var bp: BufferedPosition
-          packPositionFromBoard(b, scoreCpWhite, 0.0'f32, bp.record)
-          bp.evalWdl = evalWdlWhite
-          positions.add(bp)
-
-  return grOngoing  # unreachable
-
-const MaxFileSize = 512 * 1024 * 1024
-
-type
-  DataWriter = object
-    dir:               string
-    threadId:          int
-    fileIndex:         int
-    f:                 File
-    bytesWritten:      int64
-    recordsSinceFlush: int
-
-proc initDataWriter(dir: string; threadId: int; fileIndex: int = 0): DataWriter =
-  result.dir       = dir
-  result.threadId  = threadId
-  result.fileIndex = fileIndex
-  let path = dir / ("data_T" & $threadId & "_F" & $fileIndex & ".bin")
-  result.f = open(path, fmAppend)
-
-proc writeRecord(w: var DataWriter; rec: var BinRecord) {.inline.} =
-  if w.bytesWritten + 40 > MaxFileSize:
-    w.f.close()
-    inc w.fileIndex
-    let path = w.dir / ("data_T" & $w.threadId & "_F" & $w.fileIndex & ".bin")
-    w.f = open(path, fmWrite)
-    w.bytesWritten      = 0
-    w.recordsSinceFlush = 0
-  discard w.f.writeBuffer(addr rec, 40)
-  w.bytesWritten += 40
-  inc w.recordsSinceFlush
-  if w.recordsSinceFlush >= 2000:
-    w.f.flushFile()
-    w.recordsSinceFlush = 0
-
-proc closeWriter(w: var DataWriter) {.inline.} =
-  w.f.flushFile()
-  w.f.close()
+  return (numEntries, true)
 
 type
   SharedState = object
@@ -320,10 +157,6 @@ type
     bookPath:     string
     softNodes:    uint64
     hardNodes:    uint64
-    ttSizeMb:     int
-    lambda:       float32
-    randPlies:    int
-    filterWindow: int
     seed:         uint64
     shared:       SharedState
 
@@ -335,43 +168,38 @@ proc workerThread(args: WorkerArgs) {.thread.} =
   initThreadAttacks()
   initHistoryData()
 
-  tNNUE = NNUEState()
-
   var book: OpeningBook
   if args.bookPath.len > 0:
     book = loadOpeningBook(args.bookPath)
 
-  var rng       = initRand(int64(args.seed) + int64(args.threadId) * 1_000_003'i64)
-  var writer    = initDataWriter(args.outputDir, args.threadId)
-  var positions: seq[BufferedPosition] = @[]
-  var localStop: Atomic[bool]
-  localStop.store(false, moRelaxed)
+  var rng = initRand(int64(args.seed) + int64(args.threadId) * 1_000_003'i64)
+
+  let path = args.outputDir / ("data_" & $getMonoTime().ticks & "_t" & $args.threadId & ".vf")
+  var f = open(path, fmWrite)
+  defer: f.close()
+
+  var vb: ViriBuffer
 
   while not args.shared.stopFlag[].load(moRelaxed):
+    if args.shared.totalPositions[].load(moRelaxed) >= args.shared.targetPositions:
+      break
+
     newTTGeneration()
 
-    let res = playGame(book, rng, args.softNodes, args.hardNodes,
-                       args.randPlies, args.filterWindow, args.lambda,
-                       localStop, positions)
-    if res == grOngoing: continue
+    let (posCount, played) = dgPlayGame(book, rng, args.softNodes, args.hardNodes, vb)
+    if not played or posCount == 0:
+      continue
 
-    let gameWdl: float32 = case res
-      of grWhiteWin: 1.0'f32
-      of grBlackWin: 0.0'f32
-      else:          0.5'f32
-
-    for i in 0 ..< positions.len:
-      positions[i].record.wdl =
-        args.lambda * gameWdl + (1.0'f32 - args.lambda) * positions[i].evalWdl
-      writer.writeRecord(positions[i].record)
-
-    discard args.shared.totalPositions[].fetchAdd(int64(positions.len), moRelaxed)
+    discard args.shared.totalPositions[].fetchAdd(int64(posCount), moRelaxed)
     discard args.shared.totalGames[].fetchAdd(1'i64, moRelaxed)
+
+    if vb.buf.len > 0:
+      let n = f.writeBuffer(addr vb.buf[0], vb.buf.len)
+      doAssert n == vb.buf.len, "short write — disk full or IO error"
+      f.flushFile()
 
     if args.shared.totalPositions[].load(moRelaxed) >= args.shared.targetPositions:
       args.shared.stopFlag[].store(true, moRelaxed)
-
-  closeWriter(writer)
 
 const StateFile = "datagen_state.json"
 
@@ -403,10 +231,13 @@ when isMainModule:
   var softNodes       = 5_000'u64
   var hardNodes       = 8_000_000'u64
   var ttSizeMb        = 4
-  var lambda          = 0.5'f32
-  var randPlies       = 8
-  var filterWindow    = 300
   var seed: uint64    = 42
+
+  proc nextVal(p: var OptParser): string =
+    if p.val.len > 0: p.val
+    else:
+      p.next()
+      p.key
 
   var p = initOptParser(commandLineParams())
   while true:
@@ -415,35 +246,30 @@ when isMainModule:
     of cmdEnd: break
     of cmdShortOption, cmdLongOption:
       case p.key
-      of "book":     p.next(); bookPath        = p.key
-      of "output":   p.next(); outputDir       = p.key
-      of "target":   p.next(); targetPositions = parseBiggestInt(p.key).int64
-      of "workers":  p.next(); numWorkers      = parseInt(p.key)
-      of "sn":       p.next(); softNodes       = parseBiggestUInt(p.key)
-      of "hn":       p.next(); hardNodes       = parseBiggestUInt(p.key)
-      of "tt":       p.next(); ttSizeMb        = parseInt(p.key)
-      of "lambda":   p.next(); lambda          = parseFloat(p.key).float32
-      of "randplies":p.next(); randPlies       = parseInt(p.key)
-      of "filter":   p.next(); filterWindow    = parseInt(p.key)
-      of "seed":     p.next(); seed            = parseBiggestUInt(p.key)
+      of "book":     bookPath        = p.nextVal()
+      of "output":   outputDir       = p.nextVal()
+      of "target":   targetPositions = parseBiggestInt(p.nextVal()).int64
+      of "workers":  numWorkers      = parseInt(p.nextVal())
+      of "sn":       softNodes       = parseBiggestUInt(p.nextVal())
+      of "hn":       hardNodes       = parseBiggestUInt(p.nextVal())
+      of "tt":       ttSizeMb        = parseInt(p.nextVal())
+      of "seed":     seed            = parseBiggestUInt(p.nextVal())
       else: discard
     of cmdArgument: discard
-
-  lambda = clamp(lambda, 0.0'f32, 1.0'f32)
 
   if hardNodes > 0 and softNodes > hardNodes:
     softNodes = hardNodes
 
   initAttacks()
   initThreadAttacks()
-  initTT(16)
+  initTT(ttSizeMb)
   initTables()
   initNNUE()
   initHistoryModule()
   createDir(outputDir)
 
   echo "┌─────────────────────────────────────────┐"
-  echo "│         Gyatso Datagen v2.0             │"
+  echo "│      Gyatso Datagen v3.0 (viriformat)   │"
   echo "└─────────────────────────────────────────┘"
   echo "  Book:        ", if bookPath.len > 0: bookPath else: "<none — bookless mode>"
   echo "  Output:      ", outputDir
@@ -451,10 +277,7 @@ when isMainModule:
   echo "  Workers:     ", numWorkers
   echo "  Soft nodes:  ", softNodes, "  (exit after depth)"
   echo "  Hard nodes:  ", hardNodes, "  (mid-search abort)"
-  echo "  TT/worker:   ", ttSizeMb, " MB"
-  echo "  Lambda:      ", lambda, "  (WDL mix: game×λ + eval×(1-λ))"
-  echo "  Rand plies:  ", randPlies
-  echo "  Filter:      ", if filterWindow > 0: $filterWindow & " cp" else: "off"
+  echo "  TT size:     ", ttSizeMb, " MB"
   echo "  Seed:        ", seed
   echo "  NNUE:        embedded (", NNUE_EMBEDDED.len, " bytes)"
   when defined(avx512): echo "  SIMD:        AVX-512"
@@ -484,6 +307,7 @@ when isMainModule:
   shared.totalGames      = addr gTotalGames
   shared.targetPositions = targetPositions
   shared.stopFlag        = addr gStopFlag
+
   var threads = newSeq[Thread[WorkerArgs]](numWorkers)
   let startTime = getMonoTime()
 
@@ -494,13 +318,10 @@ when isMainModule:
     args.bookPath     = bookPath
     args.softNodes    = softNodes
     args.hardNodes    = hardNodes
-    args.ttSizeMb     = ttSizeMb
-    args.lambda       = lambda
-    args.randPlies    = randPlies
-    args.filterWindow = filterWindow
     args.seed         = seed + cast[uint64](i) * 999_983'u64
     args.shared       = shared
     createThread(threads[i], workerThread, args)
+
   var lastReportTime = getMonoTime()
   var lastReportPos  = gTotalPositions.load(moRelaxed)
   var lastSaveGames  = 0'i64
@@ -538,7 +359,7 @@ when isMainModule:
   saveState(outputDir, gTotalPositions.load(moRelaxed),
             gTotalGames.load(moRelaxed), seed)
 
-  let elapsed = (getMonoTime() - startTime).inSeconds
+  let elapsed  = (getMonoTime() - startTime).inSeconds
   let finalPos = gTotalPositions.load(moRelaxed)
   let finalGms = gTotalGames.load(moRelaxed)
   echo ""
---
+++ Gyatso/src/viriformat.nim
@@ -0,0 +1,132 @@
+import coretypes
+import board
+import bitboard
+type
+  PackedBoard* {.packed.} = object
+    occupied*:  uint64
+    pieces*:    array[16, uint8]
+    stmEp*:     uint8
+    halfmove*:  uint8
+    fullmove*:  uint16
+    score*:     int16
+    wdl*:       uint8
+    extra*:     uint8
+
+static:
+  doAssert sizeof(PackedBoard) == 32, "PackedBoard must be exactly 32 bytes"
+type
+  ViriBuffer* = object
+    buf*: seq[byte]
+
+proc reset*(vb: var ViriBuffer) {.inline.} =
+  if vb.buf.len == 0:
+    vb.buf = newSeqOfCap[byte](32 + 512 * 4 + 4)
+  else:
+    vb.buf.setLen(0)
+
+proc rookHasCastlingRights*(b: Board; sq: Square; color: Color): bool {.inline.} =
+  if color == White:
+    if sq == H1: return b.castling.hasWK()
+    if sq == A1: return b.castling.hasWQ()
+  else:
+    if sq == H8: return b.castling.hasBK()
+    if sq == A8: return b.castling.hasBQ()
+  return false
+
+proc writeBoard*(vb: var ViriBuffer; b: Board;
+                 halfmoveClock: int = -1; fullmoveCounter: int = -1) =
+  var pb: array[32, byte]
+
+  let occ = b.occupied.uint64
+  copyMem(addr pb[0], unsafeAddr occ, 8)
+
+  var occVar = b.occupied
+  var idx = 0
+  while not occVar.isEmpty():
+    let sq    = occVar.lsb()
+    occVar    = occVar and (occVar - Bitboard(1))
+    let piece = b.mailbox[sq.int]
+
+    var typ: uint8 = case piece.pieceType
+      of Pawn:   0'u8
+      of Knight: 1'u8
+      of Bishop: 2'u8
+      of Rook:   3'u8
+      of Queen:  4'u8
+      of King:   5'u8
+      else:      0'u8
+
+    if piece.pieceType == Rook and rookHasCastlingRights(b, sq, piece.color):
+      typ = 6'u8
+
+    var nibble = typ
+    if piece.color == Black: nibble = nibble or 8'u8
+
+    let byteIdx = 8 + (idx shr 1)
+    if (idx and 1) == 0:
+      pb[byteIdx] = nibble                          # low nibble
+    else:
+      pb[byteIdx] = pb[byteIdx] or (nibble shl 4)  # high nibble
+    inc idx
+
+  var ep: uint8 = if b.epSquare == NoSquare: 64'u8 else: uint8(b.epSquare.int)
+  pb[24] = ep and 0x7F'u8
+  if b.stm == Black: pb[24] = pb[24] or 0x80'u8
+
+  pb[25] = if halfmoveClock >= 0: uint8(min(halfmoveClock, 255)) else: b.halfmove
+
+  var fm = if fullmoveCounter >= 0: uint16(fullmoveCounter) else: b.fullmove
+  copyMem(addr pb[26], addr fm, 2)
+
+  let oldLen = vb.buf.len
+  vb.buf.setLen(oldLen + 32)
+  copyMem(addr vb.buf[oldLen], addr pb[0], 32)
+
+proc writeMoveEval*(vb: var ViriBuffer; m: Move; evalWhiteRel: int) =
+  var vType:  uint16 = 0
+  var vPromo: uint16 = 0
+
+  let mt     = m.moveType()
+  let fromSq = m.fromSq()
+  var toSq   = m.toSq()
+
+  case mt
+  of EnPassant:
+    vType = 1
+  of Castling:
+    vType = 2
+    if fromSq == E1:
+      toSq = if int(toSq) > int(fromSq): H1 else: A1
+    elif fromSq == E8:
+      toSq = if int(toSq) > int(fromSq): H8 else: A8
+  of Promotion:
+    vType = 3
+    vPromo = case m.promoType()
+      of PromoKnight: 0'u16
+      of PromoBishop: 1'u16
+      of PromoRook:   2'u16
+      of PromoQueen:  3'u16
+  of Normal:
+    discard
+
+  let vMove  = uint16(fromSq.int) or
+               (uint16(toSq.int) shl 6) or
+               (vPromo shl 12) or
+               (vType  shl 14)
+
+  let vScore = int16(clamp(evalWhiteRel, -32000, 32000))
+
+  var b4: array[4, byte]
+  copyMem(addr b4[0], unsafeAddr vMove,  2)
+  copyMem(addr b4[2], unsafeAddr vScore, 2)
+  let oldLen = vb.buf.len
+  vb.buf.setLen(oldLen + 4)
+  copyMem(addr vb.buf[oldLen], addr b4[0], 4)
+
+proc patchWdl*(vb: var ViriBuffer; wdl: uint8) =
+  if vb.buf.len >= 32:
+    vb.buf[30] = wdl
+  let oldLen = vb.buf.len
+  vb.buf.setLen(oldLen + 4)
+  zeroMem(addr vb.buf[oldLen], 4)
+
